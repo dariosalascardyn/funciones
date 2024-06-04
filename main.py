9fbe14bb2f20a0897d74a9f1cf5c71382fbbc6f3
dariosalascardyn <167145326+dariosalascardyn@users.noreply.github.com>
2024-06-03 21:04:48 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import os, time
+#from funciones import saludar, calcularEdad
+from funcionesactividad import *
+os.system("cls")
+
+#saludar()
+
+
+# anio_actual = 2024
+# nacimiento = int(input("ingrese año de nacimiento\n"))
+# print(f"tienes {calcularEdad(anio_actual, nacimiento)} años")
+
+
+a = int(input("ingrese numero\n"))
+b = int(input("ingrese otro numero\n"))
+print(resultado)
+
+num1 = int(input("ingrrese numero para saber si es par\n"))
+print(bandera)
